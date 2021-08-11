4be120eee73a6f67181c5c13a5029aaa31e3dec7
S-kaitei <oren.hagetaka@gmail.com>
2021-08-11 18:20:36 +0900

    instance

--- main.php
+++ main.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "処理のはじまりはじまり〜！ \n\n";
+// ファイルのロード
+require_once('./classes/Human.php');
+require_once('./classes/Enemy.php');
+
+// インスタンス化
+$tidus = new Human();
+$goblin = new Enemy();
+
+$tidus->name = "ティーダ";
+$goblin->name = "ゴブリン";
+
+echo $tidus->name . "\n";
+echo $goblin->name . "\n";
