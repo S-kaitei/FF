337ebe77f863fcc82fd74d2b145cc20d6db2a04c
S-kaitei <oren.hagetaka@gmail.com>
2021-08-11 19:02:21 +0900

    loop

--- main.php
+++ main.php
@@ -11,14 +11,23 @@ $goblin = new Enemy();
 $tidus->name = "ティーダ";
 $goblin->name = "ゴブリン";
 
-// 現在のHPの表示
-echo $tidus->name . " : " . $tidus->hitPoint . "/" . $tidus::MAX_HP . "\n";
-echo $goblin->name . " : " . $tidus->hitPoint . "/" . $goblin::MAX_HP . "\n";
-echo "\n";
+// どちらかのHPが0になるまで繰り返す
+while($tidus->hitPoint > 0 && $goblin->hitPoint > 0)
+{
+  // 現在のHPの表示
+  echo $tidus->name . " : " . $tidus->hitPoint . "/" . $tidus::MAX_HP . "\n";
+  echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HP . "\n";
+  echo "\n";
 
-// 攻撃
-$tidus->doAttack($goblin);
-echo "\n";
-$goblin->doAttack($tidus);
-echo "\n";
+  // 攻撃
+  $tidus->doAttack($goblin);
+  echo "\n";
+  $goblin->doAttack($tidus);
+  echo "\n";
+}
+
+echo "⭐️⭐️⭐️ 戦闘終了 ⭐️⭐️⭐️\n\n";
+echo $tidus->name . " : " . $tidus->hitPoint . "/" . $tidus::MAX_HP . "\n";
+echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HP . "\n";
+
 
