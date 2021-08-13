9f359a71c418dd10459f0c27e6d9711e599154b4
S-kaitei <oren.hagetaka@gmail.com>
2021-08-13 11:34:21 +0900

    edit:main,Enemy

--- main.php
+++ main.php
@@ -4,34 +4,110 @@
 require_once('./classes/Human.php');
 require_once('./classes/Enemy.php');
 require_once('./classes/Brave.php');
+require_once('./classes/BlackMage.php');
+require_once('./classes/WhiteMage.php');
 
 // インスタンス化
-$tidus = new Brave("ティーダ");
-$goblin = new Enemy("ゴブリン");
+$members = array();
+$members[] = new Brave("ティーダ");
+$members[] = new WhiteMage("ユウナ");
+$members[] = new BlackMage("ルールー");
+
+$enemies = array();
+$enemies[] = new Enemy("ゴブリン", 20);
+$enemies[] = new Enemy("ボム", 25);
+$enemies[] = new Enemy("モルボル", 30);
 
 $turn = 1;
 
+$isFinishFlg = false;
+
 // どちらかのHPが0になるまで繰り返す
-while($tidus->getHitPoint() > 0 && $goblin->getHitPoint() > 0)
+while(!$isFinishFlg)
 {
   echo "*** $turn ターン目 ***\n\n";
 
   // 現在のHPの表示
-  echo $tidus->getName() . " : " . $tidus->getHitPoint() . "/" . $tidus::MAX_HP . "\n";
-  echo $goblin->getName() . " : " . $goblin->getHitPoint() . "/" . $goblin::MAX_HP . "\n";
+  foreach($members as $member)
+  {
+    echo $member->getName() . " : " . $member->getHitPoint() . "/" . $member::MAX_HP . "\n";
+  }
+  echo "\n";
+  foreach($enemies as $enemy)
+  {
+    echo $enemy->getName() . " : " . $enemy->getHitPoint() . "/" . $enemy::MAX_HP . "\n";
+  }
   echo "\n";
 
   // 攻撃
-  $tidus->doAttack($goblin);
+  foreach($members as $member)
+  {
+    $enemyIndex = rand(0, count($enemies) -1); // 添字は0から始まるので、-1する
+    $enemy = $enemies[$enemyIndex];
+    // 白魔道士の場合、味方のオブジェクトも渡す
+    if(get_class($member) == "WhiteMage"){
+      $member->doAttackWhiteMage($enemy, $member);
+    }else{
+      $member->doAttack($enemy);
+    }
+    echo "\n";
+  }
   echo "\n";
-  $goblin->doAttack($tidus);
+
+  foreach($enemies as $enemy)
+  {
+    $memberIndex = rand(0, count($members) - 1); // 添字は0から始まるので、-1する
+    $member = $members[$memberIndex];
+    $enemy->doAttack($member);
+    echo "\n";
+  }
   echo "\n";
+
+  // 仲間の全滅チェック
+  $deathCny = 0; // HPが0以下の仲間の数
+  foreach($members as $member)
+  {
+    if($member->getHitPoint() > 0){
+      $isFinishFlg = false;
+      break;
+    }
+    $deathCny++;
+  }
+  if($deathCny === count($members))
+  {
+    $isFinishFlg = true;
+    echo "GAME OVER ....\n\n";
+    break;
+  }
+
+  // 敵の全滅チェック
+  $deathCnt = 0; // HPが0以下の敵の数
+  foreach($enemies as $enemy)
+  {
+    if($enemy->getHitPoint() > 0){
+      $isFinishFlg = false;
+      break;
+    }
+    $deathCnt++;
+  }
+  if($deathCnt === count($enemies)){
+    $isFinishFlg = true;
+    echo "♪♪♪ファンファーレ♪♪♪\n\n";
+    break;
+  }
 
   $turn++;
 }
 
 echo "⭐️⭐️⭐️ 戦闘終了 ⭐️⭐️⭐️\n\n";
-echo $tidus->getName() . " : " . $tidus->getHitPoint() . "/" . $tidus::MAX_HP . "\n";
-echo $goblin->getName() . " : " . $goblin->getHitPoint() . "/" . $goblin::MAX_HP . "\n";
+foreach($members as $member)
+{
+  echo $member->getName() . " : " . $member->getHitPoint() . "/" . $member::MAX_HP . "\n";
+}
+echo "\n";
+foreach($enemies as $enemy)
+{
+  echo $enemy->getName() . " : " . $enemy->getHitPoint() . "/" . $enemy::MAX_HP . "\n";
+}
 
 
