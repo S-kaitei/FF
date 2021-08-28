4c8641844caafdcf68ed629f9db652c4127056da
S-kaitei <oren.hagetaka@gmail.com>
2021-08-28 22:27:25 +0900

    add:Accessor

--- classes/Lives.php
+++ classes/Lives.php
@@ -16,4 +16,40 @@ class Lives
       $this->attackPoint = $attackPoint;
       $this->intelligence = $intelligence;
   }
+
+  // 名前を取得するメソッド（ゲッター）
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  // 現在HPを取得するメソッド(ゲッター)
+  public function getHitPoint()
+  {
+    $result = $this->hitPoint;
+    if($result < 0){
+      $result = 0;
+    }
+    return $result;
+  }
+
+  // 現在HPを設定するメソッド(セッター)
+  public function tookDamage($damage)
+  {
+    $this->hitPoint -= $damage;
+    // HPが0未満にならないための処理
+    if($this->hitPoint < 0){
+      $this->hitPoint = 0;
+    }
+  }
+
+  // 現在HPを設定するメソッド（セッター）
+  public function recoveryDamage($heal, $target)
+  {
+    $this->hitPoint += $heal;
+    // 最大値を超えて回復しない
+    if($this->hitPoint > $target::MAX_HP){
+      $this->hitPoint = $target::MAX_HP;
+    }
+  }
 }
