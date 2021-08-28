ca844415f5a8fbc1efd25f776f4c3e47234ba565
S-kaitei <oren.hagetaka@gmail.com>
2021-08-28 20:12:18 +0900

    add:Message.php

--- classes/Message.php
+++ classes/Message.php
@@ -2,5 +2,13 @@
 
 class Message
 {
-  
+  //ステータス表示
+  public function displayStatusMessage($objects)
+  {
+    foreach($objects as $object)
+    {
+      echo $object->getName() . " : " . $object->getHitPoint() . "/" . $object::MAX_HP . "\n";
+    }
+    echo "\n";
+  }
 }
