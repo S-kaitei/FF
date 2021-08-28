2ff4c5da6d2f0d388600aedfbeaa9648b8805f7d
S-kaitei <oren.hagetaka@gmail.com>
2021-08-28 22:10:16 +0900

    add:method

--- classes/Lives.php
+++ classes/Lives.php
@@ -8,5 +8,12 @@ class Lives
   private $attackPoint; // 攻撃力
   private $intelligence; // 魔法攻撃力
 
-  
+  // メソッド
+  public function __construct($name, $hitPoint = 50, $attackPoint = 10, $intelligence = 0)
+  {
+      $this->name = $name;
+      $this->hitPoint = $hitPoint;
+      $this->attackPoint = $attackPoint;
+      $this->intelligence = $intelligence;
+  }
 }
